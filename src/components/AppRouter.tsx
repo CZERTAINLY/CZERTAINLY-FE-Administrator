d4ae57ca0384434508854ff3010fc18defbc6f4c
David Maixner <david.maixner@morosystems.cz>
2022-10-25 13:07:26 +0200

    fix wildcard routing

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -26,34 +26,37 @@ export default function AppRouter() {
 
       () => (
 
-         <Route element={< Layout />}>
+          <>
+             <Route element={< Layout />}>
 
-            <Route path="/" element={<Navigate to="/app/home" />} />
-            <Route path="/app" element={<Navigate to="/app/home" />} />
-            <Route path="/app/home" element={<Home />} />
-            <Route path="/app/about" element={<About />} />
-            <Route path="/app/dashboard" element={<Dashboard />} />
+                <Route path="/" element={<Navigate to="/app/home" />} />
+                <Route path="/app" element={<Navigate to="/app/home" />} />
+                <Route path="/app/home" element={<Home />} />
+                <Route path="/app/about" element={<About />} />
+                <Route path="/app/dashboard" element={<Dashboard />} />
 
-            {/*<Route path="/app/users" component={Users} />*/}
-            {/*<Route path="/app/roles" component={Roles} />*/}
-            {/*<Route path="/app/audit" component={AuditLogs} />*/}
-            {/*<Route path="/app/connectors" component={Connectors} />*/}
-            {/*<Route path="/app/raprofiles" component={RaProfiles} />*/}
-            {/*<Route path="/app/credentials" component={Credentials} />*/}
-            {/*<Route path="/app/authorities" component={Authorities} />*/}
-            {/*<Route path="/app/entities" component={Entities} />*/}
-            {/*<Route path="/app/locations" component={Locations} />*/}
-            {/*<Route path="/app/acmeaccounts" component={AcmeAccounts} />*/}
-            {/*<Route path="/app/acmeprofiles" component={AcmeProfiles} />*/}
-            {/*<Route path="/app/groups" component={Groups} />*/}
-            {/*<Route path="/app/discovery" component={Discovery} />*/}
-            {/*<Route path="/app/certificates" component={Certificates} />*/}
-            {/*<Route path="/app/complianceprofiles" component={ComplianceProfiles} />*/}
+                {/*<Route path="/app/users" component={Users} />*/}
+                {/*<Route path="/app/roles" component={Roles} />*/}
+                {/*<Route path="/app/audit" component={AuditLogs} />*/}
+                {/*<Route path="/app/connectors" component={Connectors} />*/}
+                {/*<Route path="/app/raprofiles" component={RaProfiles} />*/}
+                {/*<Route path="/app/credentials" component={Credentials} />*/}
+                {/*<Route path="/app/authorities" component={Authorities} />*/}
+                {/*<Route path="/app/entities" component={Entities} />*/}
+                {/*<Route path="/app/locations" component={Locations} />*/}
+                {/*<Route path="/app/acmeaccounts" component={AcmeAccounts} />*/}
+                {/*<Route path="/app/acmeprofiles" component={AcmeProfiles} />*/}
+                {/*<Route path="/app/groups" component={Groups} />*/}
+                {/*<Route path="/app/discovery" component={Discovery} />*/}
+                {/*<Route path="/app/certificates" component={Certificates} />*/}
+                {/*<Route path="/app/complianceprofiles" component={ComplianceProfiles} />*/}
 
-            <Route path="/app/userprofile" element={<UserProfileDetail />} />
-            <Route path="/app/userprofile/edit" element={<UserProfileEdit />} />
+                <Route path="/app/userprofile" element={<UserProfileDetail />} />
+                <Route path="/app/userprofile/edit" element={<UserProfileEdit />} />
 
-         </Route >
+             </Route >
+             <Route path="*" element={<Navigate to="/app/home"/>}/>
+          </>
 
       ),
       []
@@ -74,7 +77,9 @@ export default function AppRouter() {
                profile && appRoutes
             }
 
-            <Route path="*" element={<Spinner active={true} />} />
+            {
+               !profile && <Route path="*" element={<Spinner active={true}/>}/>
+            }
 
          </Routes>
 
