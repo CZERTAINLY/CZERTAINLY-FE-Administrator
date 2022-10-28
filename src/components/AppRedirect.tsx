895a07b32f76bfaafc52bd4257bc04db1ecda33a
Fis <frantisek.novak@atomsoftwarestudios.com>
2022-10-28 21:53:20 +0200

    Typo fix

--- src/components/AppRedirect.tsx
+++ src/components/AppRedirect.tsx
@@ -40,7 +40,7 @@ export default function AppRedirect() {
             window.location.toString().substring(window.location.origin.length)
          );
 
-         navigate(`${__ENV__.LOGIN_URL}}?redirect=${url}`);
+         navigate(`${__ENV__.LOGIN_URL}?redirect=${url}`);
 
       },
       [dispatch, navigate, unauthorized]
