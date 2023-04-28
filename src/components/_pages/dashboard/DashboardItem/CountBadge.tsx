5406c90538e2fd82331f5bec3f2d99de29a44112
David Maixner <david.maixner@morosystems.cz>
2023-04-28 09:38:09 +0200

    Dashboard statistics redirects
    close #276

--- src/components/_pages/dashboard/DashboardItem/CountBadge.tsx
+++ src/components/_pages/dashboard/DashboardItem/CountBadge.tsx
@@ -1,14 +1,22 @@
 import Widget from "components/Widget";
+import { Link } from "react-router-dom";
 import { Col, Row } from "reactstrap";
 
 interface Props {
     data?: number;
     title: string;
+    link: string;
 }
 
-function CountBadge({ data, title }: Props) {
+function CountBadge({ data, title, link }: Props) {
     return (
-        <Widget title={<p style={{ fontWeight: 700 }}>{title}</p>}>
+        <Widget
+            title={
+                <Link to={link}>
+                    <p style={{ fontWeight: 700 }}>{title}</p>
+                </Link>
+            }
+        >
             <Row className={`justify-content-between mt-3 gx-0`}>
                 <Col sm={8} className={"d-flex align-items-center"}>
                     <h3 className={"fw-semi-bold mb-0"}>{data}</h3>
---
+++ src/components/_pages/dashboard/DashboardItem/DonutChart.tsx
@@ -0,0 +1,72 @@
+import Widget from "components/Widget";
+import { EntityType, actions } from "ducks/filters";
+import ReactApexChart from "react-apexcharts";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { SearchFilterModel } from "types/certificate";
+import { DashboardDict } from "types/statisticsDashboard";
+import { getLabels, getValues } from "utils/dashboard";
+
+interface Props {
+    title: string;
+    data?: DashboardDict;
+    entity: EntityType;
+    redirect: string;
+    onSetFilter: (index: number, labels: string[]) => SearchFilterModel[];
+}
+
+function DonutChart({ title, data = {}, entity, redirect, onSetFilter: onLegendClick }: Props) {
+    const labels = getLabels(data);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const options: ApexCharts.ApexOptions = {
+        labels,
+        theme: {
+            mode: "light",
+            monochrome: {
+                enabled: false,
+                color: "#e31e25",
+                shadeTo: "light",
+                shadeIntensity: 0.8,
+            },
+        },
+        fill: {
+            type: "gradient",
+        },
+        legend: {
+            onItemHover: { highlightDataSeries: true },
+        },
+        dataLabels: {
+            formatter: function (val: any, opts: any) {
+                return opts.w.config.series[opts.seriesIndex];
+            },
+        },
+        chart: {
+            toolbar: {
+                show: true,
+                offsetX: 0,
+                offsetY: -40,
+                tools: {
+                    download: true,
+                },
+            },
+            events: {
+                legendClick(_chart: any, index: any, _options: any) {
+                    dispatch(
+                        actions.setCurrentFilters({ entity, currentFilters: index !== undefined ? onLegendClick(index, labels) : [] }),
+                    );
+                    navigate(redirect);
+                },
+            },
+        },
+    };
+
+    return (
+        <Widget title={<p style={{ fontWeight: 700 }}>{title}</p>}>
+            <ReactApexChart options={options} series={getValues(data)} type="donut" height="100%" width="100%" />
+        </Widget>
+    );
+}
+
+export default DonutChart;
