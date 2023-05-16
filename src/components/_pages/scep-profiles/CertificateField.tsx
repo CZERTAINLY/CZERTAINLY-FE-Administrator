ba1e64be56c8a181225721ca5c2ad5312b9131f1
David Maixner <david.maixner@morosystems.cz>
2023-05-16 14:23:00 +0200

    Update RA profile for existing SCEP profile does not send selected RA profile
    close #319

---
+++ src/components/_pages/scep-profiles/CertificateField.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useMemo } from "react";
+import { Field, useForm, useFormState } from "react-final-form";
+import Select from "react-select";
+import { FormGroup, Label } from "reactstrap";
+import { CertificateListResponseModel } from "types/certificate";
+
+import { validateRequired } from "utils/validators";
+
+interface Props {
+    certificates: CertificateListResponseModel[] | undefined;
+}
+
+export default function CertificateField({ certificates }: Props) {
+    const form = useForm();
+    const formState = useFormState();
+
+    useEffect(() => {
+        if (
+            formState.values["certificate"] &&
+            certificates &&
+            !certificates?.find((c) => c.uuid === formState.values["certificate"]?.value)
+        ) {
+            form.change("certificate", undefined);
+        }
+    }, [certificates, formState.values, form]);
+
+    const optionsForCertificates = useMemo(() => {
+        return certificates?.map((certificate) => ({
+            value: certificate.uuid,
+            label: `${certificate.commonName} (${certificate.serialNumber})`,
+        }));
+    }, [certificates]);
+
+    return (
+        <Field name="certificate" type="select" validate={validateRequired()}>
+            {({ input, meta }) => (
+                <FormGroup>
+                    <Label for="certificate">CA Certificate</Label>
+                    <Select
+                        {...input}
+                        id="certificate"
+                        maxMenuHeight={140}
+                        menuPlacement="auto"
+                        options={optionsForCertificates}
+                        placeholder="Select to change CA Certificate if needed"
+                        isClearable={true}
+                    />
+                </FormGroup>
+            )}
+        </Field>
+    );
+}
