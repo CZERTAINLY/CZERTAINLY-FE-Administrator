57ccfaa7ef96412fe7e3797b530a049b50b75d6d
3keyroman <roman.cinkais@3key.company>
2023-08-16 07:55:26 +0200

    Add default values

--- cypress.config.js
+++ cypress.config.js
@@ -6,5 +6,10 @@ module.exports = defineConfig({
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
+    },
+    env: {
+        ADMIN_URL: 'http://localhost:3000/',
+        ADMIN_USERNAME: 'czertainly-admin',
+        ADMIN_PASSWORD: 'your-strong-password'
     }
 })
