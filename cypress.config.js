8cae0bebc50fd0d5823c458dd80d753156cee33e
3keyroman <46850604+3keyroman@users.noreply.github.com>
2023-08-28 16:24:02 +0200

    Include cypress for e2e and component testing (#462)

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+    e2e: {
+        experimentalStudio: true,
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+    },
+    env: {
+        ADMIN_URL: 'http://localhost:3000/',
+        ADMIN_USERNAME: 'czertainly-admin',
+        ADMIN_PASSWORD: 'your-strong-password'
+    }
+})
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,18 @@
+describe(
+    'template spec',
+    {},
+    () => {
+      it('passes', () => {
+        cy.visit(Cypress.env('ADMIN_URL'))
+        /* ==== Generated with Cypress Studio ==== */
+        cy.get('button').click();
+        cy.get('#username').clear();
+        cy.get('#username').type(Cypress.env('ADMIN_USERNAME'));
+        cy.get('#password').clear();
+        cy.get('#password').type(Cypress.env('ADMIN_PASSWORD'));
+        cy.get('.submit').click();
+        cy.get('.LinksGroup_headerLinkActive__qKWEL > div > .LinksGroup_menuLabel__rz1AM').should('have.text', 'Dashboard');
+        cy.get('.Header_adminName__EGy0a').should('have.text', Cypress.env('ADMIN_USERNAME'));
+        /* ==== End Cypress Studio ==== */
+      })
+})
