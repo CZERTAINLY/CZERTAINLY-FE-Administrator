1013dd2f64159724a84236cc7321b10dc8f74a29
Abhijit Bansode <bansodeabhijit1997@gmail.com>
2023-09-18 18:07:44 +0530

    added missing styles and dialog component test

--- cypress/component/ProgressButton.cy.jsx
+++ cypress/component/ProgressButton.cy.jsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
 import ProgressButton from "../../src/components/ProgressButton/index";
+import "../../src/resources/styles/theme.scss";
 
-describe("StatusBadge.cy.jsx", () => {
-    it("playground", () => {
-        cy.mount(<ProgressButton title="Test Progress Button" />);
+const TestProgressButton = () => {
+    const [inProgress, setInProgress] = useState(false);
+
+    const toggleProgress = () => {
+        console.log("toggleProgress");
+        setInProgress(!inProgress);
+
+        setTimeout(() => {
+            setInProgress(false);
+        }, 750);
+    };
+
+    return <ProgressButton inProgressTitle="Loading" title="Test Progress Button" inProgress={inProgress} onClick={toggleProgress} />;
+};
+
+describe("Progress Button component", () => {
+    it("should render progress button with text 'Test Progress Button'", () => {
+        cy.mount(<TestProgressButton />);
         cy.get("button").should("have.text", "Test Progress Button");
         cy.get("button").click();
     });
--- cypress/component/WidgetLock.cy.jsx
+++ cypress/component/WidgetLock.cy.jsx
@@ -1,8 +1,8 @@
 import WidgetLock from "../../src/components/WidgetLock/index";
-// import WidgetLock from "../../src/components/WidgetLock/index";
+import "../../src/resources/styles/theme.scss";
 
-describe("WidgetLock.cy.jsx", () => {
-    it("playground", () => {
+describe("Widget Lock component", () => {
+    it("should render widget lock with lock title 'Test Lock' & lock text 'Test lock text", () => {
         cy.mount(<WidgetLock lockTitle="Test lock" lockText="Test lock text" />);
         cy.get("h5").should("have.text", "Test lock");
         cy.get("p").should("have.text", "Test lock text");
