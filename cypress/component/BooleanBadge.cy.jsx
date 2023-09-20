9782b12b427fd5f5b448d5f7ee43188e4a083831
Abhijit Bansode <bansodeabhijit1997@gmail.com>
2023-09-20 18:36:17 +0530

    update boolean badge custom table & tab layout component tests

--- cypress/component/BooleanBadge.cy.jsx
+++ cypress/component/BooleanBadge.cy.jsx
@@ -1,7 +1,7 @@
 import BooleanBadge from "../../src/components/BooleanBadge/BooleanBadge.tsx";
 import "../../src/resources/styles/theme.scss";
 
-describe("Boolean badge component", () => {
+describe("Boolean badge component with true value", () => {
     it("should render 'Yes' for true value", () => {
         cy.mount(<BooleanBadge value={true} />);
         cy.get("span").should("have.text", "Yes");
@@ -9,7 +9,7 @@ describe("Boolean badge component", () => {
     });
 });
 
-describe("BooleanBadge.cy.jsx", () => {
+describe("Boolean badge component with true value", () => {
     it("should render 'No' for false value", () => {
         cy.mount(<BooleanBadge value={false} />);
         cy.get("span").should("have.text", "No");
---
+++ cypress/component/TabLayout.cy.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "../../src/resources/styles/theme.scss";
+
+import TabLayout from "../../src/components/Layout/TabLayout";
+
+const TestTabLayout = () => {
+    return (
+        <TabLayout
+            tabs={[
+                {
+                    title: <span id="tabOneTitle">Tab 1</span>,
+                    content: <span id="tabOneContent">Tab 1 content</span>,
+                },
+                {
+                    title: <span id="tabTwoTitle">Tab 2</span>,
+                    content: <span id="tabTwoContent">Tab 2 content</span>,
+                },
+            ]}
+        />
+    );
+};
+
+describe("Tab Layout component", () => {
+    it("should render tab layout with 2 tabs", () => {
+        cy.mount(<TestTabLayout />);
+        cy.get("#tabOneContent").should("have.text", "Tab 1 content");
+        cy.get("#tabTwoContent").should("have.text", "Tab 2 content");
+        cy.wait(1000);
+        cy.get("#tabTwoTitle").should("have.text", "Tab 2").click();
+        cy.wait(1000);
+        cy.get("#tabOneTitle").should("have.text", "Tab 1").click();
+    });
+});
