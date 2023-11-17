72277b8163ffce55d80c91d22d2b2e046fa364ad
Abhijit Bansode <bansodeabhijit1997@gmail.com>
2023-11-17 19:07:10 +0530

    update add button styling

--- src/components/CustomSelectComponent/index.tsx
+++ src/components/CustomSelectComponent/index.tsx
@@ -1,5 +1,7 @@
+import cx from "classnames";
 import { MenuProps, components } from "react-select";
 import { Button } from "reactstrap";
+import style from "./CustomSelectComponent.module.scss";
 
 interface CustomSelectComponentProps extends MenuProps {
     onAddNew: () => void;
@@ -10,9 +12,8 @@ const CustomSelectComponent: React.FC<CustomSelectComponentProps> = ({ onAddNew,
         <components.Menu {...props}>
             {props.children}
             <div className="d-flex justify-content-start ps-2 py-2">
-                <Button className="p-2" color="primary" outline size="sm" onClick={onAddNew}>
-                    <span className="p-1 fs-6">Add new</span>
-                    <i className="fa fa-add mx-2" />
+                <Button color="white" className={style.customSelectAddButton} onClick={onAddNew}>
+                    <i className={cx("fa fa-add", style.customSelectAddIcon)} />
                 </Button>
             </div>
         </components.Menu>
