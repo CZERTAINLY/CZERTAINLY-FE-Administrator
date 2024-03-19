e3422b61f9ad403d7b9256918c445d0498a3d06e
Abby <bansodeabhijit1997@gmail.com>
2024-03-19 19:28:56 +0530

    Attribute viewer and Custom attributes widget component tests (#599)

--- cypress/component/Attributes/ComplianceRuleAttributeViewer/ComplianceRuleAttributeViewer.cy.tsx
+++ cypress/component/Attributes/ComplianceRuleAttributeViewer/ComplianceRuleAttributeViewer.cy.tsx
@@ -3,18 +3,57 @@ import '../../../../src/resources/styles/theme.scss';
 import { complianceRuleAttributeViewerProps } from './mock-data';
 
 describe('Compliance Attribute Viewer', () => {
-    it('should render ComplianceRuleAttributeViewer without descriptors', () => {
+    beforeEach(() => {
         cy.mount(<ComplianceRuleAttributeViewer attributes={complianceRuleAttributeViewerProps.attributes} />);
+    });
+    it('should render correct number of rows,columns and data elements', () => {
+        cy.get('th').should('have.length', 2);
+        cy.get('tr').should('have.length', 3);
+        cy.get('td').should('have.length', 4);
+    });
+
+    it('should render correct heading', () => {
+        cy.get('th').eq(0).should('contain.text', 'Name');
+        cy.get('th').eq(1).should('contain.text', 'Value');
+    });
+
+    it('should render correct data', () => {
+        cy.get('td').eq(0).should('contain.text', 'test-name-1');
+        cy.get('td').eq(1).should('contain.text', 'test-data-1');
+        cy.get('td').eq(2).should('contain.text', 'test-name-2');
+        cy.get('td').eq(3).should('contain.text', 'true');
     });
 });
 
 describe('Compliance Attribute Viewer with descriptors', () => {
-    it('should render ComplianceRuleAttributeViewer with descriptors', () => {
+    beforeEach(() => {
         cy.mount(
             <ComplianceRuleAttributeViewer
                 attributes={complianceRuleAttributeViewerProps.attributes}
                 descriptorAttributes={complianceRuleAttributeViewerProps.descriptorAttributes}
             />,
         );
+    });
+
+    it('should render correct number of rows,columns and data elements', () => {
+        cy.get('th').should('have.length', 2);
+        cy.get('tr').should('have.length', 5);
+        cy.get('td').should('have.length', 8);
+    });
+
+    it('should render correct heading', () => {
+        cy.get('th').eq(0).should('contain.text', 'Name');
+        cy.get('th').eq(1).should('contain.text', 'Value');
+    });
+
+    it('should render correct data', () => {
+        cy.get('td').eq(0).should('contain.text', 'test-name-1');
+        cy.get('td').eq(1).should('contain.text', 'test-data-1');
+        cy.get('td').eq(2).should('contain.text', 'test-name-2');
+        cy.get('td').eq(3).should('contain.text', 'true');
+        cy.get('td').eq(4).should('contain.text', 'test-descriptor-name-1');
+        cy.get('td').eq(5).should('contain.text', 'test-data-1');
+        cy.get('td').eq(6).should('contain.text', 'test-descriptor-name-2');
+        cy.get('td').eq(7).should('contain.text', 'test-data-2');
     });
 });
