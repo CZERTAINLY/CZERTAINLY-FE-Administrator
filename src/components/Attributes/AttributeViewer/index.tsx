fe3cc8536758f44aca8f30e00b8f411dc7a6b23d
Abhijit Bansode <bansodeabhijit1997@gmail.com>
2024-03-28 18:52:56 +0530

    fix build error

--- src/components/Attributes/AttributeViewer/index.tsx
+++ src/components/Attributes/AttributeViewer/index.tsx
@@ -201,7 +201,7 @@ export default function AttributeViewer({
                 />,
             ],
         }),
-        [getAttributesTableData],
+        [getAttributesTableData, resourceEnum],
     );
 
     const getButtons = useCallback(
