8a4a87fb44a53718553d2c2e52b6e23d159b4951
Ivan-Koshan <ivan.koshan@keenethics.com>
2025-04-02 18:07:14 +0300

    Add copy button to copy public key value

--- src/components/_pages/auth-settings/JwkSetKeysTable.tsx
+++ src/components/_pages/auth-settings/JwkSetKeysTable.tsx
@@ -1,12 +1,15 @@
 import CustomTable, { TableDataRow, TableHeader } from 'components/CustomTable';
 import React, { useCallback, useMemo } from 'react';
+import { Button } from 'reactstrap';
 import { JwkModel } from 'types/auth-settings';
+import { useCopyToClipboard } from 'utils/common-hooks';
 
 type Props = {
     jwkSetKeys?: JwkModel[];
 };
 
 const JwkSetKeysTable = ({ jwkSetKeys }: Props) => {
+    const copyToClipboard = useCopyToClipboard();
     const headers: TableHeader[] = useMemo(
         () => [
             {
@@ -21,6 +24,11 @@ const JwkSetKeysTable = ({ jwkSetKeys }: Props) => {
                 id: 'algorithm',
                 content: 'Algorithm',
             },
+            {
+                id: 'moreInfo',
+                content: '',
+                width: '2%',
+            },
         ],
         [],
     );
@@ -34,46 +42,76 @@ const JwkSetKeysTable = ({ jwkSetKeys }: Props) => {
                 id: 'algorithm',
                 content: 'Value',
             },
+            {
+                id: 'moreInfo',
+                content: '',
+                width: '2%',
+            },
         ],
         [],
     );
 
-    const createDetailDataForTable = useCallback((jwkKeySet: JwkModel): TableDataRow[] => {
-        return [
-            {
-                id: 'kid',
-                columns: ['Key ID', jwkKeySet.kid],
-            },
-            {
-                id: 'type',
-                columns: ['Key Type', jwkKeySet.keyType ?? ''],
-            },
-            {
-                id: 'use',
-                columns: ['Key Use', jwkKeySet.use ?? ''],
-            },
-            {
-                id: 'algorithm',
-                columns: ['Algorithm', jwkKeySet.algorithm ?? ''],
-            },
-            {
-                id: 'value',
-                columns: [
-                    'Base64 Encoded Value',
-                    <span
-                        key="value"
-                        style={{
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                            wordBreak: 'break-all',
-                        }}
-                    >
-                        {jwkKeySet.publicKey}
-                    </span>,
-                ],
-            },
-        ];
-    }, []);
+    const renderCopyKeyButton = useCallback(
+        (publicKey: string) => (
+            <Button
+                className="btn btn-link py-0 px-1 ms-2"
+                color="white"
+                title="Public Key"
+                key="copy"
+                onClick={() =>
+                    copyToClipboard(
+                        publicKey,
+                        'Public key content was copied to clipboard',
+                        'Failed to copy public key content to clipboard',
+                    )
+                }
+            >
+                <i className="fa fa-copy" style={{ color: 'auto' }} />
+            </Button>
+        ),
+        [copyToClipboard],
+    );
+
+    const createDetailDataForTable = useCallback(
+        (jwkKeySet: JwkModel): TableDataRow[] => {
+            return [
+                {
+                    id: 'kid',
+                    columns: ['Key ID', jwkKeySet.kid],
+                },
+                {
+                    id: 'type',
+                    columns: ['Key Type', jwkKeySet.keyType ?? ''],
+                },
+                {
+                    id: 'use',
+                    columns: ['Key Use', jwkKeySet.use ?? ''],
+                },
+                {
+                    id: 'algorithm',
+                    columns: ['Algorithm', jwkKeySet.algorithm ?? ''],
+                },
+                {
+                    id: 'value',
+                    columns: [
+                        'Base64 Encoded Value',
+                        <span
+                            key="value"
+                            style={{
+                                overflow: 'hidden',
+                                textOverflow: 'ellipsis',
+                                wordBreak: 'break-all',
+                            }}
+                        >
+                            {jwkKeySet.publicKey}
+                        </span>,
+                        renderCopyKeyButton(jwkKeySet.publicKey),
+                    ],
+                },
+            ];
+        },
+        [renderCopyKeyButton],
+    );
 
     const data: TableDataRow[] = useMemo(
         () =>
@@ -83,7 +121,7 @@ const JwkSetKeysTable = ({ jwkSetKeys }: Props) => {
                       (key) =>
                           ({
                               id: key.kid,
-                              columns: [key.kid, key.keyType, key.algorithm],
+                              columns: [key.kid, key.keyType, key.algorithm, renderCopyKeyButton(key.publicKey)],
                               detailColumns: [
                                   <></>,
                                   <CustomTable
@@ -95,7 +133,7 @@ const JwkSetKeysTable = ({ jwkSetKeys }: Props) => {
                               ],
                           }) as TableDataRow,
                   ),
-        [jwkSetKeys, createDetailDataForTable, detailHeaders],
+        [jwkSetKeys, createDetailDataForTable, detailHeaders, renderCopyKeyButton],
     );
     return <CustomTable hasDetails={true} headers={headers} data={data} />;
 };
