a30379086d2fee1876159418b7e79b0db708ec0e
soloviovmax <solovyov.amron@gmail.com>
2025-12-23 15:41:39 +0200

    Refactor AppLogin component to fix redirect logic

--- src/components/AppLogin/AppLogin.tsx
+++ src/components/AppLogin/AppLogin.tsx
@@ -1,17 +1,14 @@
-import { useCallback, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useSearchParams } from 'react-router';
 
 export default function AppLogin() {
     const [searchParams] = useSearchParams();
 
-    const redirect = useCallback(() => {
-        const redirect = encodeURIComponent(searchParams.get('redirect') || '');
-        window.location.href = `${__ENV__.LOGIN_URL}?redirect=${redirect}`;
-    }, [searchParams]);
-
     useEffect(() => {
-        redirect();
-    }, [redirect]);
+        const redirectParam = encodeURIComponent(searchParams.get('redirect') || '');
+        window.location.href = `${__ENV__.LOGIN_URL}?redirect=${redirectParam}`;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return null;
 }
