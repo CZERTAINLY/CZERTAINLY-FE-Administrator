ddf3c3711df14d164b664f966b48173d460e2e71
soloviovmax <solovyov.amron@gmail.com>
2026-01-16 13:10:53 +0200

    Certificate Details: Download dropdown is non-interactive on Request tab (#1090)

--- src/components/_pages/certificates/detail/CertificateRequestContent.tsx
+++ src/components/_pages/certificates/detail/CertificateRequestContent.tsx
@@ -17,6 +17,7 @@ import { selectors as enumSelectors, getEnumLabel } from 'ducks/enums';
 import Asn1Dialog from '../Asn1Dialog/Asn1Dialog';
 import { AttributeResponseModel } from 'types/attributes';
 import { Download, Copy } from 'lucide-react';
+import Dropdown from 'components/Dropdown';
 
 interface Props {
     certificate: CertificateDetailResponseModel | undefined;
@@ -37,73 +38,47 @@ export default function CertificateRequestContent({ certificate, isBusy, getFres
 
     const downloadCSRDropDown = useMemo(
         () => (
-            <div className="hs-dropdown relative inline-flex">
-                <button
-                    id="hs-dropdown-custom-icon-trigger"
-                    type="button"
-                    className="hs-dropdown-toggle py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-800"
-                    title="Download"
-                >
-                    <Download size={16} aria-hidden="true" />
-                    <svg
-                        className="hs-dropdown-open:rotate-180 size-4"
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                    >
-                        <path d="m6 9 6 6 6-6" />
-                    </svg>
-                </button>
-
-                <div
-                    className="hs-dropdown-menu transition-[opacity,margin] duration hs-dropdown-open:opacity-100 opacity-0 hidden min-w-60 bg-white shadow-md rounded-lg p-2 mt-2 dark:bg-neutral-800 dark:border dark:border-neutral-700"
-                    aria-labelledby="hs-dropdown-custom-icon-trigger"
-                >
-                    <div className="flex">
-                        <button
-                            className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-                            onClick={() =>
-                                downloadFile(
-                                    formatPEM(certificate?.certificateRequest?.content ?? '', true),
-                                    fileNameToDownload + '_CSR' + '.pem',
-                                )
-                            }
-                        >
-                            PEM (.pem)
-                        </button>
-                        <Copy
-                            className="ml-2"
-                            size={16}
-                            onClick={() => {
-                                if (!certificate?.certificateRequest?.content) return;
-                                copyToClipboard(
-                                    formatPEM(certificate?.certificateRequest?.content ?? '', true),
-                                    'Certificate request content was copied to clipboard',
-                                    'Failed to copy certificate request content to clipboard',
-                                );
-                            }}
-                        />
-                    </div>
-
-                    <button
-                        className="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-                        onClick={() =>
+            <Dropdown
+                title={
+                    <>
+                        <Download size={16} aria-hidden="true" />
+                        <span className="sr-only">Download</span>
+                    </>
+                }
+                items={[
+                    {
+                        title: 'PEM (.pem)',
+                        onClick: () =>
+                            downloadFile(
+                                formatPEM(certificate?.certificateRequest?.content ?? '', true),
+                                fileNameToDownload + '_CSR' + '.pem',
+                            ),
+                    },
+                    {
+                        title: (
+                            <div className="flex items-center gap-x-2">
+                                <span>Copy PEM to clipboard</span>
+                            </div>
+                        ),
+                        onClick: () => {
+                            if (!certificate?.certificateRequest?.content) return;
+                            copyToClipboard(
+                                formatPEM(certificate?.certificateRequest?.content ?? '', true),
+                                'Certificate request content was copied to clipboard',
+                                'Failed to copy certificate request content to clipboard',
+                            );
+                        },
+                    },
+                    {
+                        title: 'REQ (.req)',
+                        onClick: () =>
                             downloadFile(
                                 Buffer.from(certificate?.certificateRequest?.content ?? '', 'base64'),
                                 fileNameToDownload + '_CSR' + '.req',
-                            )
-                        }
-                    >
-                        REQ (.req)
-                    </button>
-                </div>
-            </div>
+                            ),
+                    },
+                ]}
+            />
         ),
         [certificate, fileNameToDownload, copyToClipboard],
     );
