8b5121cbe911b56ea7906927cd545ae96174bd34
soloviovmax <solovyov.amron@gmail.com>
2026-01-30 12:54:02 +0200

    Certificates by Type - Filter tag is not updated after opening Certificate Details

--- src/components/_pages/dashboard/index.tsx
+++ src/components/_pages/dashboard/index.tsx
@@ -1,7 +1,7 @@
 import Spinner from 'components/Spinner';
 import { selectors as enumSelectors } from 'ducks/enums';
 import { actions as certificatesActions, selectors as certificatesSelectors } from 'ducks/certificates';
-import { EntityType } from 'ducks/filters';
+import { actions as filterActions, EntityType } from 'ducks/filters';
 import { actions, selectors } from 'ducks/statisticsDashboard';
 import { useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
@@ -154,7 +154,10 @@ function Dashboard() {
                         title={'Certificates by Type'}
                         data={dashboard?.certificateStatByType}
                         entity={EntityType.CERTIFICATE}
-                        onSetFilter={(_index, _labels) => []}
+                        onSetFilter={(_index, _labels) => {
+                            dispatch(filterActions.setPreservedFilters({ entity: EntityType.CERTIFICATE, preservedFilters: [] }));
+                            return [];
+                        }}
                         redirect="../certificates"
                     />
                 )}
