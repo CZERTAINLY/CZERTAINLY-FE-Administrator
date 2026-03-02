ad0eb237008088dfbb256326cb610ee0cdde5159
Ivo Raisr <ivosh@ivosh.net>
2026-03-02 20:23:46 +0100

    Temporarily run the Sonar action from the HEAD/PR branch

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -64,6 +64,7 @@ export default defineConfig({
     },
     projects: [
         { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+        { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
         { name: 'webkit', use: { ...devices['Desktop Safari'] } },
     ],
     reporter: [
