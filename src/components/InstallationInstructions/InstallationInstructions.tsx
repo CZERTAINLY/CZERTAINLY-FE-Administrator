67a7669e29734fdd54d99fe9c37463a794ec9c6c
Ivo Raisr <ivosh@ivosh.net>
2026-03-20 11:55:33 +0100

    Scroll horizontally installation instructions

--- src/components/InstallationInstructions/InstallationInstructions.tsx
+++ src/components/InstallationInstructions/InstallationInstructions.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import cn from 'classnames';
 import { Copy } from 'lucide-react';
+
 import Button from 'components/Button';
 import { useCopyToClipboard } from 'utils/common-hooks';
+
+import styles from './installationInstructions.module.scss';
 
 interface Props {
     title: string;
@@ -31,12 +34,8 @@ export const InstallationInstructions = ({ title, instructions, className, id, '
         ));
 
     return (
-        <section
-            id={id}
-            data-testid={dataTestId}
-            className={cn('border border-[#1C2740] bg-[#0E1728] rounded-xl overflow-hidden', className)}
-        >
-            <div className="flex items-center justify-between px-10 py-5 border-b border-[#1C2740] text-[#A8A8A8]">
+        <section id={id} data-testid={dataTestId} className={cn(styles.root, className)}>
+            <div className={styles.header}>
                 <h5 className="text-lg">{title}</h5>
                 <Button
                     variant="transparent"
@@ -49,7 +48,7 @@ export const InstallationInstructions = ({ title, instructions, className, id, '
                 </Button>
             </div>
 
-            <div className="px-10 py-5 flex flex-col gap-1.5 bg-[#0B1220] text-[#C0CAF5]">{renderInstructions()}</div>
+            <div className={styles.instructions}>{renderInstructions()}</div>
         </section>
     );
 };
