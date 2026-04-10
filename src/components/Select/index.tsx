bcba0074ea7ef3ca0ac3cad29c2f515bcceb40e4
soloviovmax <solovyov.amron@gmail.com>
2026-04-10 15:39:24 +0300

    Selects fix

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -4,6 +4,15 @@ import Button from 'components/Button';
 import cn from 'classnames';
 import { X } from 'lucide-react';
 
+const HS_SELECT_WRAPPER_CLASSES =
+    'relative ps-2 pe-9 min-h-11 flex items-center flex-wrap w-full bg-white border border-gray-200 rounded-lg text-start text-sm hover:bg-gray-50 focus-within:ring-2 focus-within:ring-blue-500 focus-within:border-blue-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:hover:bg-neutral-800 [&>*:not(.hs-select-dropdown)]:max-w-full';
+
+const HS_SELECT_TAGS_ITEM_TEMPLATE =
+    '<div class="max-w-full min-w-0 flex flex-nowrap items-center relative z-10 bg-white border border-gray-200 rounded-full p-1 pl-2.5 m-1 dark:bg-neutral-800 dark:border-neutral-600"><div class="hs-tooltip [--placement:top] inline-block min-w-0 flex-1"><div class="hs-tooltip-toggle truncate min-w-0 text-[var(--dark-gray-color)] dark:text-neutral-200 cursor-default block" data-title></div><span class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 invisible transition-opacity inline-block absolute z-[110] py-1 px-2 bg-[var(--tooltip-background-color)] text-xs font-medium text-white rounded-md shadow-2xs dark:bg-neutral-700" data-tooltip-content role="tooltip"></span></div><div class="inline-flex shrink-0 justify-center items-center size-5 ms-1.5 rounded-full bg-gray-200 hover:bg-gray-300 focus:outline-none text-gray-600 hover:text-gray-800 dark:bg-neutral-600 dark:hover:bg-neutral-500 dark:text-neutral-300 cursor-pointer" data-remove><svg class="shrink-0 size-3" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg></div></div>';
+
+const HS_SELECT_OPTION_TEMPLATE =
+    '<div class="flex justify-between items-center w-full text-[var(--dark-gray-color)] min-w-0"><div class="hs-tooltip [--placement:top] inline-block min-w-0 flex-1"><span class="hs-tooltip-toggle truncate block min-w-0 cursor-pointer" data-title></span><span class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 invisible transition-opacity inline-block absolute z-[110] py-1 px-2 bg-[var(--tooltip-background-color)] text-xs font-medium text-white rounded-md shadow-2xs dark:bg-neutral-700" data-tooltip-content role="tooltip"></span></div><span class="hidden hs-selected:block shrink-0"><svg class="shrink-0 size-3.5 text-blue-600 dark:text-blue-500 ml-2" xmlns="http:.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg></span></div>';
+
 export type SingleValue<T> = T | undefined;
 export type MultiValue<T> = T[] | undefined;
 
@@ -389,12 +398,10 @@ function Select({
                     data-hs-select={JSON.stringify({
                         ...(isMulti && {
                             mode: 'tags',
-                            wrapperClasses:
-                                'relative ps-2 pe-9 min-h-11 flex items-center flex-wrap w-full bg-white border border-gray-200 rounded-lg text-start text-sm hover:bg-gray-50 focus-within:ring-2 focus-within:ring-blue-500 focus-within:border-blue-500 dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:hover:bg-neutral-800 [&>*:not(.hs-select-dropdown)]:max-w-full',
+                            wrapperClasses: HS_SELECT_WRAPPER_CLASSES,
                             tagsInputClasses:
                                 'py-2.5 px-1 flex-1 min-w-[80px] bg-transparent border-0 text-[var(--dark-gray-color)] placeholder-gray-400 focus:ring-0 text-sm outline-none dark:text-neutral-200 dark:placeholder-neutral-500 order-1',
-                            tagsItemTemplate:
-                                '<div class="max-w-full min-w-0 flex flex-nowrap items-center relative z-10 bg-white border border-gray-200 rounded-full p-1 pl-2.5 m-1 dark:bg-neutral-800 dark:border-neutral-600"><div class="hs-tooltip [--placement:top] inline-block min-w-0 flex-1"><div class="hs-tooltip-toggle truncate min-w-0 text-[var(--dark-gray-color)] dark:text-neutral-200 cursor-default block" data-title></div><span class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 invisible transition-opacity inline-block absolute z-[110] py-1 px-2 bg-[var(--tooltip-background-color)] text-xs font-medium text-white rounded-md shadow-2xs dark:bg-neutral-700" data-tooltip-content role="tooltip"></span></div><div class="inline-flex shrink-0 justify-center items-center size-5 ms-1.5 rounded-full bg-gray-200 hover:bg-gray-300 focus:outline-none text-gray-600 hover:text-gray-800 dark:bg-neutral-600 dark:hover:bg-neutral-500 dark:text-neutral-300 cursor-pointer" data-remove><svg class="shrink-0 size-3" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg></div></div>',
+                            tagsItemTemplate: HS_SELECT_TAGS_ITEM_TEMPLATE,
                         }),
                         hasSearch: hasSearch,
                         searchPlaceholder: 'Search...',
@@ -412,8 +419,7 @@ function Select({
                         ...(dropdownScope && { dropdownScope }),
                         optionClasses:
                             'hs-select-option-row py-2 px-3 w-full text-sm cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-hidden focus:bg-gray-100 hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 dark:bg-neutral-900 dark:hover:bg-neutral-800 dark:text-neutral-200 dark:focus:bg-neutral-800 overflow-hidden',
-                        optionTemplate:
-                            '<div class="flex justify-between items-center w-full text-[var(--dark-gray-color)] min-w-0"><div class="hs-tooltip [--placement:top] inline-block min-w-0 flex-1"><span class="hs-tooltip-toggle truncate block min-w-0 cursor-pointer" data-title></span><span class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 invisible transition-opacity inline-block absolute z-[110] py-1 px-2 bg-[var(--tooltip-background-color)] text-xs font-medium text-white rounded-md shadow-2xs dark:bg-neutral-700" data-tooltip-content role="tooltip"></span></div><span class="hidden hs-selected:block shrink-0"><svg class="shrink-0 size-3.5 text-blue-600 dark:text-blue-500 ml-2" xmlns="http:.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg></span></div>',
+                        optionTemplate: HS_SELECT_OPTION_TEMPLATE,
                         extraMarkup:
                             '<div class="absolute top-1/2 end-3 -translate-y-1/2"><svg class="shrink-0 size-3.5 dark:text-neutral-500 " xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="m7 15 5 5 5-5"/><path d="m7 9 5-5 5 5"/></svg></div>',
                         closeOnSelect: !isMulti,
