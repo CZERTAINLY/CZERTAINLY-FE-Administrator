e3d79b9b5c055ebcea286afd356f75ca75766899
soloviovmax <solovyov.amron@gmail.com>
2026-05-04 11:06:43 +0300

    Tests fix

--- src/components/PagedList/PagedListSkeleton.spec.tsx
+++ src/components/PagedList/PagedListSkeleton.spec.tsx
@@ -1,14 +1,16 @@
 import { test, expect } from '../../../playwright/ct-test';
+import { createMockStore, withProviders } from 'utils/test-helpers';
 import PagedListSkeleton from './PagedListSkeleton';
 
 test.describe('PagedListSkeleton', () => {
     test('should render 10 skeleton rows', async ({ mount }) => {
         const component = await mount(<PagedListSkeleton />);
-        await expect(component.getByTestId('skeleton-row')).toHaveCount(10);
+        await expect(component.getByTestId('table-skeleton-row')).toHaveCount(10);
     });
 
     test('should render filter widget skeleton when hasFilter is true', async ({ mount }) => {
-        const component = await mount(<PagedListSkeleton hasFilter />);
+        const store = createMockStore();
+        const component = await mount(withProviders(<PagedListSkeleton hasFilter />, { store }));
         await expect(component.getByTestId('filter-widget-skeleton')).toHaveCount(1);
     });
 
@@ -19,7 +21,7 @@ test.describe('PagedListSkeleton', () => {
 
     test('should render the correct number of button skeletons', async ({ mount }) => {
         const component = await mount(<PagedListSkeleton buttonsCount={3} />);
-        const header = component.getByTestId('paged-list-skeleton').locator('section').first().locator('> div').first();
+        const header = component.locator('section').first().locator('> div').first();
         const buttons = header.locator('> div').last().locator('> div');
         await expect(buttons).toHaveCount(3);
     });
